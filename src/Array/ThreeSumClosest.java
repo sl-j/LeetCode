1072b47f56f187d26a5a288622b596e50a072d48
sl-j <1319209005@qq.com>
2021-11-15 20:18:30 +0800

    2021-11-13

--- src/Array/ThreeSumClosest.java
+++ src/Array/ThreeSumClosest.java
@@ -3,6 +3,17 @@ package Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+/**
+ * 给你一个长度为 n 的整数数组 nums 和 一个目标值 target。请你从 nums 中选出三个整数，使它们的和与 target 最接近。
+ *
+ * 返回这三个数的和。
+ *
+ * 假定每组输入只存在恰好一个解。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/3sum-closest
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
 public class ThreeSumClosest {
     public static void main(String[] args) {
         System.out.println(threeSumClosest(new int[]{-1,0,1,1,55},3));
