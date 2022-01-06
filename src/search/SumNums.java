0d151b62f50c8b3dd2be10fbdd6296a081f78a26
sl-j <1319209005@qq.com>
2022-01-06 15:13:56 +0800

    2021-11-13

---
+++ src/search/SumNums.java
@@ -0,0 +1,16 @@
+package search;
+
+/**
+ * 求 1+2+...+n ，要求不能使用乘除法、for、while、if、else、switch、case等关键字及条件判断语句（A?B:C）。
+ */
+public class SumNums {
+    public static void main(String[] args) {
+        System.out.println(sumNums(9));
+    }
+    static int res = 0;
+    public static int sumNums(int n) {
+        boolean x = n > 1 && sumNums(n - 1) > 0;
+        res += n;
+        return res;
+    }
+}
