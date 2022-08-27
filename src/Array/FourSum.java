35c1b3e398a37fddae3d10d5d657a921ec8b99ef
sl-j <1319209005@qq.com>
2022-08-27 20:16:21 +0800

    2022-08-08

--- src/Array/FourSum.java
+++ src/Array/FourSum.java
@@ -2,30 +2,30 @@ package Array;
 
 
 import java.util.*;
+import java.util.concurrent.ThreadPoolExecutor;
 
 public class FourSum {
     public static void main(String[] args) {
     }
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        List<Integer> res = new ArrayList<>();
+        PriorityQueue<Integer> queue = new PriorityQueue<>((o1, o2) -> {
+            if(Math.abs(o1 - x) == Math.abs(o2 - x)) return o2 - o1;
+                else return Math.abs(o2 - x) - Math.abs(o1 - x);
+        });
 
-    private static double slove(double money){
-        double res = 0;
-        if(money <= 100000) res = money * 0.05;
-        else if(money > 100000 && money < 200000){
-            res = 100000 * 0.05 + (money - 100000) * 0.04;
-        }else if(money >= 200000 && money < 400000){
-            res = 100000 * 0.05 + (200000 - 100000) * 0.04 + (money - 200000) * 0.05;
-        }else if(money >= 400000 && money < 600000){
-            res = 100000 * 0.05 + (200000 - 100000) * 0.04 + (400000 - 200000) * 0.05 + (money - 400000) * 0.03;
-        }else if(money >= 600000 && money < 1000000){
-            res = 100000 * 0.05 + (200000 - 100000) * 0.04 + (400000 - 200000) * 0.05 + (600000 - 400000) * 0.03 + (money - 600000) * 0.015;
-        }else{
-            res = 100000 * 0.05 + (200000 - 100000) * 0.04 + (400000 - 200000) * 0.05 + (600000 - 400000) * 0.03 + (1000000 - 600000) * 0.015
-                    + (money - 1000000) * 0.01;
+
+
+        for(int num : arr){
+            queue.offer(num);
+            if(queue.size() > k) queue.poll();
+        }
+
+        while(!queue.isEmpty()){
+            res.add(queue.poll());
         }
 
         return res;
     }
-
-
 
 }
--- src/Array/Main.java
+++ src/Array/Main.java
@@ -6,63 +6,52 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
-class Solution {
+class Main {
 
-    /* Write Code Here */
-    public static int findMinDifference(String[] timePoints) {
-        int res = 24*62;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] strs = br.readLine().split(" ");
+        int n = Integer.parseInt(strs[0]);
+        int m = Integer.parseInt(strs[1]);
+        int[][] value = new int[m][3];
 
-        for(int i = 0;i < timePoints.length;i++){
-            for(int j = i + 1;j < timePoints.length;j++){
-                res = Math.min(res,help(timePoints[i],timePoints[j]));
+
+        int index = 0;
+        for(int i = 0;i < 3;i++){
+            String[] tmp = br.readLine().split(" ");
+            for(int j = 0;j < tmp.length;j++){
+                value[j][i] = Integer.parseInt(tmp[j]);
             }
+        }
+
+        System.out.println(slove(value,n));
+
+    }
+
+    private static long slove(int[][] value,int n){
+        PriorityQueue<int[]> queue = new PriorityQueue<>((int[] o1,int[] o2) -> o1[2] - o2[2]);
+        for(int[] num : value){
+            queue.offer(num);
+        }
+
+        Set<Integer> set = new HashSet<>();
+        long res = 0;
+        while(!queue.isEmpty()){
+            int[] num = queue.poll();
+            int v = num[0];
+            int u = num[1];
+            if(set.contains(v) && set.contains(u)) continue;
+            res += num[2];
+            set.add(v);
+            set.add(u);
+
+            if(set.size() == n) break;
         }
 
         return res;
-
     }
 
-    private static int help(String s1,String s2){
-        int num1 = Integer.parseInt(s1.substring(3,s1.length()));
-        int num2 = Integer.parseInt(s2.substring(3,s2.length()));
-        int max = 24*60;
 
-        int num3 = Integer.parseInt(s1.substring(0,2));
-        int num4 = Integer.parseInt(s2.substring(0,2));
 
-        int n1 = num3 * 60 + num1;
-        int n2 = num4 * 60 + num2;
 
-        if(n1 < n2){
-            int tmp = n1;
-            n1 = n2;
-            n2 = tmp;
-        }
-
-        return Math.min(n1- n2,max - n1 + n2);
-    }
-
-    public static void main(String[] args){
-        Scanner in = new Scanner(System.in);
-        int res;
-
-        int timePoints_size = 0;
-        timePoints_size = in.nextInt();
-        if (in.hasNextLine())
-            in.nextLine();
-        String[] timePoints = new String[timePoints_size];
-        String timePoints_item;
-        for(int timePoints_i = 0; timePoints_i < timePoints_size; timePoints_i++) {
-            try {
-                timePoints_item = in.nextLine();
-            } catch (Exception e) {
-                timePoints_item = null;
-            }
-            timePoints[timePoints_i] = timePoints_item;
-        }
-
-        res = findMinDifference(timePoints);
-        System.out.println(String.valueOf(res));
-
-    }
 }
